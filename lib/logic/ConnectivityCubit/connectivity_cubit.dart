50cc3b665f208fa10fc126b02295d6863f736830
HoualefAkram <akram1232004@gmail.com>
2024-03-09 23:43:41 +0100

    added connection with HC-05

--- lib/logic/ConnectivityCubit/connectivity_cubit.dart
+++ lib/logic/ConnectivityCubit/connectivity_cubit.dart
@@ -1,32 +1,46 @@
+import 'dart:developer';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pfc_sgc/enums/amplifier_circuit.dart';
-
+import 'package:pfc_sgc/services/bluetooth_service.dart';
 part 'connectivity_state.dart';
 
 class ConnectivityCubit extends Cubit<ConnectivityState> {
   ConnectivityCubit() : super(ConnectivityStateUninitialized());
-
+  final BluetoothService bluetoothService = BluetoothService();
   Future<void> initialize() async {
-    // TODO: check if app is connected
-
-    // TODO: check selected type
-
-    // Initialize
     emit(
       ConnectivityStateDisconnected(),
     );
   }
 
-  Future<void> connect() async {}
+  Future<void> connect() async {
+    if (bluetoothService.isConnected) return;
+    final bool connect = await bluetoothService.connect();
+    log("connect: $connect");
+    if (connect) {
+      emit(
+        ConnectivityStateConnected(
+          amplifierCircuit: AmplifierCircuit.weinAOP,
+        ),
+      );
+    }
+  }
+
+  Future<void> sendData(String data) async {
+    await bluetoothService.sendData(data);
+  }
 
   Future<void> changeCircuit({
     required AmplifierCircuit amplifierCircuit,
   }) async {
     // TODO: Change circuit
 
-    // TODO: emit new state
-    emit(ConnectivityStateConnected(
-      amplifierCircuit: amplifierCircuit,
-    ));
+    //  emit new state
+    emit(
+      ConnectivityStateConnected(
+        amplifierCircuit: amplifierCircuit,
+      ),
+    );
   }
 }
---
+++ lib/services/bluetooth_service.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+
+class BluetoothService {
+  BluetoothConnection? _bluetoothConnection;
+  bool get isConnected => _bluetoothConnection?.isConnected ?? false;
+
+  Future<bool> connect() async {
+    _bluetoothConnection =
+        await BluetoothConnection.toAddress("98:DA:50:02:45:71");
+    if (isConnected) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<void> sendData(String data) async {
+    if (_bluetoothConnection == null) return;
+    _bluetoothConnection!.output.add(ascii.encode(data));
+  }
+}
